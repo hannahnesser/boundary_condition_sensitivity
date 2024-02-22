a5c73525a5eed01a39b206333a2d50736a740859
Hannah Nesser <hnesser@g.harvard.edu>
2024-02-21 16:09:04 -0800

    Updated to more realistic emissions conditions

--- python/settings.py
+++ python/settings.py
@@ -5,8 +5,8 @@ import forward_model as fm
 
 ## Standard inversion settings
 # Random state
-random_state  = 872
-rs = np.random.RandomState(random_state)
+random_state  = 372 #872
+# rs = np.random.RandomState(random_state)
 
 # Inversion dimensions
 nstate        = 20
@@ -14,7 +14,7 @@ nobs_per_cell = 15
 nobs = nstate*nobs_per_cell
 
 # Courant number
-Cmax          = 0.5
+Cmax          = 1
 
 # Grid cell length (km)
 L             = 25
@@ -28,14 +28,14 @@ total_t       = 300/24
 
 # True quantities
 BC_t          = 1900 # Boundary condition (ppb)
-x_abs_t       = 100 # Emissions (ppb/day)
+x_abs_t       = 30 # Emissions (ppb/day)
 
 # Prior errors (ppb/day)
-sa            = 0.5
+sa            = 0.75
 sa_BC         = 0.05
 
 # Observational errors (ppb)
-so            = 15
+so            = 1
 
 # Plotting materials
 xp            = np.arange(1, nstate + 1)
